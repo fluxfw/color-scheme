48100ce4827e93f8f7735bb2c23f7637456ce738
fluxfw <fluxfw@fluxlabs.ch>
2023-03-20 13:54:31 +0100

    Simplify

--- src/FluxColorScheme.mjs
+++ src/FluxColorScheme.mjs
@@ -1,7 +1,10 @@
+import { COLOR_SCHEME_CSS_PROPERTY_PREFIX } from "./ColorScheme/COLOR_SCHEME_CSS_PROPERTY_PREFIX.mjs";
 import { COLOR_SCHEME_LOCALIZATION_MODULE } from "./Localization/_LOCALIZATION_MODULE.mjs";
+import { COLOR_SCHEME_SETTINGS_KEY } from "./Settings/COLOR_SCHEME_SETTINGS_KEY.mjs";
+import { COLOR_SCHEME_DARK, COLOR_SCHEME_LIGHT } from "./ColorScheme/COLOR_SCHEME.mjs";
+import { VARIABLE_ACCENT, VARIABLE_ACCENT_FOREGROUND, VARIABLE_ACCENT_FOREGROUND_RGB, VARIABLE_ACCENT_RGB, VARIABLE_BACKGROUND, VARIABLE_BACKGROUND_RGB, VARIABLE_FOREGROUND, VARIABLE_FOREGROUND_RGB } from "./ColorScheme/VARIABLE.mjs";
 
 /** @typedef {import("./ColorScheme/ColorScheme.mjs").ColorScheme} ColorScheme */
-/** @typedef {import("./ColorScheme/Port/ColorSchemeService.mjs").ColorSchemeService} ColorSchemeService */
 /** @typedef {import("./ColorScheme/ColorSchemeWithSystemColorScheme.mjs").ColorSchemeWithSystemColorScheme} ColorSchemeWithSystemColorScheme */
 /** @typedef {import("../../flux-css-api/src/FluxCssApi.mjs").FluxCssApi} FluxCssApi */
 /** @typedef {import("../../flux-localization-api/src/FluxLocalizationApi.mjs").FluxLocalizationApi} FluxLocalizationApi */
@@ -16,10 +19,6 @@ export class FluxColorScheme {
      * @type {string[] | null}
      */
     #additional_variables;
-    /**
-     * @type {ColorSchemeService | null}
-     */
-    #color_scheme_service = null;
     /**
      * @type {ColorScheme[]}
      */
@@ -87,15 +86,7 @@ export class FluxColorScheme {
      * @returns {Promise<void>}
      */
     async init() {
-        if (this.#system_color_scheme_detector === null) {
-            this.#system_color_scheme_detector ??= await this.#getSystemColorSchemeDetector();
-
-            this.#system_color_scheme_detector.addEventListener("change", () => {
-                this.renderColorScheme(
-                    true
-                );
-            });
-        }
+        await this.#getSystemColorSchemeDetector();
 
         await this.#flux_localization_api.addModule(
             `${__dirname}/Localization`,
@@ -118,70 +109,117 @@ export class FluxColorScheme {
      * @returns {Promise<string>}
      */
     async getAccent() {
-        return (await this.#getColorSchemeService()).getAccent();
+        return this.getVariable(
+            VARIABLE_ACCENT
+        );
     }
 
     /**
      * @returns {Promise<string>}
      */
     async getAccentForeground() {
-        return (await this.#getColorSchemeService()).getAccentForeground();
+        return this.getVariable(
+            VARIABLE_ACCENT_FOREGROUND
+        );
     }
 
     /**
      * @returns {Promise<string>}
      */
     async getAccentForegroundRgb() {
-        return (await this.#getColorSchemeService()).getAccentForegroundRgb();
+        return this.getVariable(
+            VARIABLE_ACCENT_FOREGROUND_RGB
+        );
     }
 
     /**
      * @returns {Promise<string>}
      */
     async getAccentRgb() {
-        return (await this.#getColorSchemeService()).getAccentRgb();
+        return this.getVariable(
+            VARIABLE_ACCENT_RGB
+        );
     }
 
     /**
      * @returns {Promise<string>}
      */
     async getBackground() {
-        return (await this.#getColorSchemeService()).getBackground();
+        return this.getVariable(
+            VARIABLE_BACKGROUND
+        );
     }
 
     /**
      * @returns {Promise<string>}
      */
     async getBackgroundRgb() {
-        return (await this.#getColorSchemeService()).getBackgroundRgb();
+        return this.getVariable(
+            VARIABLE_BACKGROUND_RGB
+        );
     }
 
     /**
      * @returns {Promise<ColorSchemeWithSystemColorScheme>}
      */
     async getColorScheme() {
-        return (await this.#getColorSchemeService()).getColorScheme();
+        let color_scheme_name = await this.#flux_settings_api.get(
+            COLOR_SCHEME_SETTINGS_KEY,
+            ""
+        );
+
+        const system_color_scheme = this.#system_color_schemes !== null && color_scheme_name === "";
+        if (system_color_scheme) {
+            color_scheme_name = ((await this.#getSystemColorSchemeDetector()).matches ? this.#system_color_schemes[COLOR_SCHEME_DARK] : this.#system_color_schemes[COLOR_SCHEME_LIGHT]) ?? "";
+        }
+
+        const color_scheme = this.#color_schemes.find(_color_scheme => _color_scheme.name === color_scheme_name) ?? this.#color_schemes[0] ?? {};
+
+        return {
+            color_scheme: "",
+            name: "",
+            ...color_scheme,
+            system_color_scheme
+        };
     }
 
     /**
      * @returns {Promise<string>}
      */
     async getForeground() {
-        return (await this.#getColorSchemeService()).getForeground();
+        return this.getVariable(
+            VARIABLE_FOREGROUND
+        );
     }
 
     /**
      * @returns {Promise<string>}
      */
     async getForegroundRgb() {
-        return (await this.#getColorSchemeService()).getForegroundRgb();
+        return this.getVariable(
+            VARIABLE_FOREGROUND_RGB
+        );
     }
 
     /**
      * @returns {Promise<SelectColorSchemeElement>}
      */
     async getSelectColorSchemeElement() {
-        return (await this.#getColorSchemeService()).getSelectColorSchemeElement();
+        return (await import("./ColorScheme/SelectColorSchemeElement.mjs")).SelectColorSchemeElement.new(
+            await this.getColorScheme(),
+            this.#color_schemes,
+            this.#flux_css_api,
+            this.#flux_localization_api,
+            color_scheme_name => {
+                this.#setColorScheme(
+                    color_scheme_name
+                );
+            },
+            await this.#getVariables(
+                true
+            ),
+            this.#system_color_schemes
+        );
     }
 
     /**
@@ -189,9 +227,7 @@ export class FluxColorScheme {
      * @returns {Promise<string>}
      */
     async getVariable(variable) {
-        return (await this.#getColorSchemeService()).getVariable(
-            variable
-        );
+        return getComputedStyle(document.documentElement).getPropertyValue(`${COLOR_SCHEME_CSS_PROPERTY_PREFIX}${variable}`).trim();
     }
 
     /**
@@ -199,32 +235,79 @@ export class FluxColorScheme {
      * @returns {Promise<void>}
      */
     async renderColorScheme(only_if_system_color_scheme = false) {
-        await (await this.#getColorSchemeService()).renderColorScheme(
-            only_if_system_color_scheme
-        );
-    }
+        const color_scheme = await this.getColorScheme();
 
-    /**
-     * @returns {Promise<ColorSchemeService>}
-     */
-    async #getColorSchemeService() {
-        this.#color_scheme_service ??= (await import("./ColorScheme/Port/ColorSchemeService.mjs")).ColorSchemeService.new(
-            this.#color_schemes,
-            this.#flux_css_api,
-            this.#flux_localization_api,
-            this.#flux_settings_api,
-            () => this.#system_color_scheme_detector,
-            this.#system_color_schemes,
-            this.#additional_variables
-        );
+        if (only_if_system_color_scheme && !color_scheme.system_color_scheme) {
+            return;
+        }
 
-        return this.#color_scheme_service;
+        for (const key of Array.from(document.documentElement.style).filter(_key => _key.startsWith(COLOR_SCHEME_CSS_PROPERTY_PREFIX))) {
+            document.documentElement.style.removeProperty(key);
+        }
+        for (const variable of await this.#getVariables()) {
+            document.documentElement.style.setProperty(`${COLOR_SCHEME_CSS_PROPERTY_PREFIX}${variable}`, `var(${COLOR_SCHEME_CSS_PROPERTY_PREFIX}${color_scheme.name}-${variable})`);
+        }
+
+        const color_scheme_meta = document.head.querySelector("meta[name=color-scheme]") ?? document.createElement("meta");
+        color_scheme_meta.content = color_scheme.color_scheme !== "" ? color_scheme.color_scheme : COLOR_SCHEME_LIGHT;
+        color_scheme_meta.name = "color-scheme";
+        if (!color_scheme_meta.isConnected) {
+            document.head.appendChild(color_scheme_meta);
+        }
+
+        const theme_color_meta = document.head.querySelector("meta[name=theme-color]") ?? document.createElement("meta");
+        theme_color_meta.content = await this.getAccent();
+        theme_color_meta.name = "theme-color";
+        if (!theme_color_meta.isConnected) {
+            document.head.appendChild(theme_color_meta);
+        }
     }
 
     /**
      * @returns {Promise<MediaQueryList>}
      */
     async #getSystemColorSchemeDetector() {
-        return (await this.#getColorSchemeService()).getSystemColorSchemeDetector();
+        if (this.#system_color_scheme_detector === null) {
+            this.#system_color_scheme_detector ??= await this.#getSystemColorSchemeDetector();
+
+            this.#system_color_scheme_detector.addEventListener("change", () => {
+                this.renderColorScheme(
+                    true
+                );
+            });
+        }
+
+        return this.#system_color_scheme_detector;
+    }
+
+    /**
+     * @param {boolean} only_default
+     * @returns {Promise<string[]>}
+     */
+    async #getVariables(only_default = false) {
+        return Array.from(new Set([
+            VARIABLE_ACCENT,
+            VARIABLE_ACCENT_FOREGROUND,
+            VARIABLE_ACCENT_FOREGROUND_RGB,
+            VARIABLE_ACCENT_RGB,
+            VARIABLE_BACKGROUND,
+            VARIABLE_BACKGROUND_RGB,
+            VARIABLE_FOREGROUND,
+            VARIABLE_FOREGROUND_RGB,
+            ...(!only_default ? this.#additional_variables : null) ?? []
+        ]));
+    }
+
+    /**
+     * @param {string} color_scheme_name
+     * @returns {Promise<void>}
+     */
+    async #setColorScheme(color_scheme_name) {
+        await this.#flux_settings_api.store(
+            COLOR_SCHEME_SETTINGS_KEY,
+            color_scheme_name
+        );
+
+        await this.renderColorScheme();
     }
 }
