1da9882e2c6b62adb4507a7a2f052c8f53c364f4
fluxfw <fluxfw@bluewin.ch>
2024-08-05 15:13:39 +0200

    Fix

--- library/ColorScheme.mjs
+++ library/ColorScheme.mjs
@@ -523,7 +523,7 @@ export class ColorScheme extends EventTarget {
             }
         );
         const change_detail_string = JSON.stringify(change_detail);
-        if (!(change_event === null ? this.#last_change_detail === change_detail_string : change_event)) {
+        if (!(change_event === null ? this.#last_change_detail !== change_detail_string : change_event)) {
             return;
         }
         this.#last_change_detail = change_detail_string;
